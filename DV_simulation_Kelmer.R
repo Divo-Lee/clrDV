4636770afa2557a32487dbf9dbc9bb3094a5fb80
Hongxiang Li <81342027+Divo-Lee@users.noreply.github.com>
2023-07-12 03:57:59 +0800

    DV_simulation_Kelmer.R

--- DV_simulation_Kelmer.R
+++ DV_simulation_Kelmer.R
@@ -24,9 +24,10 @@
 # BiocManager::install("missMethyl", force = T)
 # BiocManager::install("cqn")
 # devtools::install_github("zjdaye/MDSeq")
+# devtools::install_github("aedanr/DiffDist", force=T)
 
 library(polyester); library(compositions); library(edgeR)
-library(MDSeq); library(missMethyl); library(gamlss); library(clrDV)
+library(MDSeq); library(missMethyl); library(gamlss); library(clrDV); library(DiffDist)
 
 ################################
 ### GSE150318_counts.csv.gz 
